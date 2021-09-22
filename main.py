84147565699fe0996fac51240286fb28748323ea
dumdoo <rohailfaisal14@gmail.com>
2021-09-22 16:33:50 +0500

    Initial Commit

---
+++ main.py
@@ -0,0 +1,38 @@
+import discord
+from discord.ext import commands
+import time
+from dotenv import load_dotenv
+from os import getenv
+import os
+from rich import print
+
+load_dotenv(".env")
+
+TOKEN = getenv("TOKEN")
+PREFIX = "]"
+intents = discord.Intents.default()
+client = commands.Bot(command_prefix=None, case_insensitive=True, intents=intents)
+
+
+@commands.command()
+@client.check
+async def globally_block_dms(ctx):
+    return ctx.guild is not None
+
+
+for filename in os.listdir("./cogs"):
+    if filename.endswith(".py"):
+        client.load_extension(f"cogs.{filename[:-3]}")
+
+
+@client.event
+async def on_ready():
+    print(f"[green bold][{time.strftime('%T')}] Logged in as {client.user}")
+    await client.change_presence(
+        activity=discord.Activity(
+            type=discord.ActivityType.competing, name=f"a future Codejam!"
+        )
+    )
+
+
+client.run(TOKEN)
